862958e7abee05ebbcfda29afd70c77f5e488877
leila-orouji <leila.orooji@gmail.com>
2020-09-25 20:43:28 +0330

    grunt

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
     $('[data-toggle = "tooltip"]').tooltip();
 })
+
+
+
+// $(document).on('click', '#carousel-pause', function() { $('#mycarousel').carousel('pause'); });
+
+$(document).ready(function(){
+    // console.log(' increase the interval carousel in jquery');
+    // $("#mycarousel").carousel({interval: 1000})
+    $('#carouselButton').on('click',function(){
+       if ($('#carouselButton').children('span').hasClass('fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+       }else{
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+       }      
+    })
+})
